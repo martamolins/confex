7338fd0326ad23775354d342a8c9c4f0dccf4794
martamolins <86597760+martamolins@users.noreply.github.com>
2021-07-01 01:50:08 +0200

    Update test_ANM.py

--- test/test_ANM/test_ANM.py
+++ test/test_ANM/test_ANM.py
@@ -1,7 +1,7 @@
 import unittest
 import os
 
-os. chdir('/home/marta/Documentos/TFM/ProgramaTFM')
+
 from simtk.openmm.app import *
 from simtk.openmm import *
 from simtk.unit import *
@@ -14,17 +14,17 @@ from ANM.ANM_Simulation import  *
 
 class TestANM(unittest.TestCase):
     def test_anm(self):
-        with open('/home/marta/Documentos/TFM/ProgramaTFM/test/test_ANM/input/test_anm.json') as file:
+        with open(os.getcwd()+'/test/test_ANM/input/test_anm.json') as file:
             Information = json.load(file)
-        if os.path.isfile('/home/marta/Documentos/TFM/ProgramaTFM/test/test_ANM/output/Trajectory_ANM.pdb'):
-            os.remove('/home/marta/Documentos/TFM/ProgramaTFM/test/test_ANM/output/Trajectory_ANM.pdb') 
+        if os.path.isfile(os.getcwd()+'/test/test_ANM/output/Trajectory_ANM.pdb'):
+            os.remove(os.getcwd()+'/test/test_ANM/output/Trajectory_ANM.pdb') 
         S = Information["Simulation"]
         Sim = "P"
         ANMSimulation(S)
         
         
-        f1 = "/home/marta/Documentos/TFM/ProgramaTFM/test/test_ANM/input/Trajectory_ANM.pdb"
-        f2 = "/home/marta/Documentos/TFM/ProgramaTFM/test/test_ANM/output/Trajectory_ANM.pdb"
+        f1 = os.getcwd()+"/test/test_ANM/input/Trajectory_ANM.pdb"
+        f2 = os.getcwd()+"/test/test_ANM/output/Trajectory_ANM.pdb"
           
         positions1 = np.array(PDBFile(f1).positions.value_in_unit(nanometer))
         positions2 = np.array(PDBFile(f2).positions.value_in_unit(nanometer))
